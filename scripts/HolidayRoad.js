eff32cb6af52d0c5a0161234d672cfb081aeb5e3
Renee Doll <renee@reneedoll.com>
2022-10-05 13:45:56 -0500

    Removed center tags

--- scripts/HolidayRoad.js
+++ scripts/HolidayRoad.js
@@ -4,13 +4,13 @@ import { CreateItinerary } from "./preview"
 
 export const HolidayRoad = () => {
     return `
-    <center><h1>Holiday Road - Itinerary Form</h1>
+    <h1>Holiday Road - Itinerary Form</h1>
     <section class="itineraryForm">
         ${CreateItinerary()}
     </section>
     <section class="completedItinerary">
         <h2>Saved Itineraries</h2>
         ${SavedItineraries()}
-    </section></center>
+    </section>
     `
 }
