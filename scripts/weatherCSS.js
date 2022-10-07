3b4b4c6d15e2e2f7b8beb57a9985e929c48b2d5c
Renee Doll <renee@reneedoll.com>
2022-10-07 13:03:50 -0500

    Changes to CSS

--- scripts/weatherCSS.js
+++
@@ -1,6 +0,0 @@
-const change1 = () => {
-    var elem = document.getElementById("weather-1");
-        elem.style.backgroundImage = "url(https://img.icons8.com/offices/2x/sun.png)"
-        elem.style.backgroundColor = "white";
-        elem.style.borderColor = "white";
-    }
