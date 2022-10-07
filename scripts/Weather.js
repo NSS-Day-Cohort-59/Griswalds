f573a6b7927ce91b1a73d363ff53c27dfaccb111
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 11:33:08 -0500

    WIP

--- scripts/Weather.js
+++ scripts/Weather.js
@@ -1,11 +1,25 @@
-import { getTransientState } from "./dataAccess.js"
+import { getTransientState, getParks, fetchWeatherForecast, getWeather } from "./dataAccess.js"
 
 //Function generates html for the weather display
 export const showWeather = () => {
     const transientState = getTransientState()
+    const parks = getParks()
+    let html = ""
     //if a park is selected...
-    if ()
+    if (transientState.parkId) {
+        //find park in applicationState
+        let foundPark = parks.find(park => park.id === transientState.parkId)
+        //call fetchWeather with matching lat. and long.
+        fetchWeatherForecast(foundPark.latitude, foundPark.longitude)
+            .then(() => {
+                //.then append string containing html
+                const forecastArray = getWeather()
+                forecastArray.forEach((forecast) => {
+                    html += `
+                    <div class="daily_forecast">
+                        
+                    </div>`
+                })
+            })
+    }
 }
-//find park in applicationState
-//call fetchWeather with matching lat. and long.
-//.then return string containing html
