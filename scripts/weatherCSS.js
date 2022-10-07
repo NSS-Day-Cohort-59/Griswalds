9bcf6e7089f2445e9915ef245428b7bda140b47e
Renee Doll <renee@reneedoll.com>
2022-10-07 11:19:54 -0500

    add updates

---
+++ scripts/weatherCSS.js
@@ -0,0 +1,6 @@
+const change1 = () => {
+    var elem = document.getElementById("weather-1");
+        elem.style.backgroundImage = "url(https://img.icons8.com/offices/2x/sun.png)"
+        elem.style.backgroundColor = "white";
+        elem.style.borderColor = "white";
+    }
